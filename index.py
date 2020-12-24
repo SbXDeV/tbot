52cbb456fd5256f9467bea5216e86f3d5a79e0da
SbXDeV <37068098+SbXDeV@users.noreply.github.com>
2020-12-24 20:07:55 +0400

    Add files via upload

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ print("Бот начал работу: ")
 
 bot = telebot.TeleBot(token)
 
+url = "<a href=\"https://telegra.ph/leylarelax-usloviya-12-21\">?</a>"
 name = ["Марина", "Августа", "Жанна", "Оксана", "Злата", "Полина", "Кира", "Алиса", "Алла", "Альбина", "Анастасия", "Анфиса", "Виола", "Влада", "Зарина", "Катерина", "Кристина", "Ксения", "Лариса", "Каролина", "София", "Мария", "Надежда", "Надя", "Юлия", "Кристина", "Вика", "Наталия", "Марина", "Анна", "Наталья", "Елена", "Евгения", "Диана", "Регина", "Ева", "Жасмин", "Зарина", "Инесса", "Мэри", "Олеся", "Ульяна", "Хана", "Хельга", "Яна", "Эля", "Маринет", "Азалия", "Виктория", "Элла"]
 photo = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg", "41.jpg", "42.jpg", "43.jpg", "44.jpg", "45.jpg", "46.jpg", "47.jpg", "48.jpg", "49.jpg", "50.jpg", "51.jpg", "52.jpg", "53.jpg", "54.jpg", "55.jpg", "56.jpg"]
 desc = [
@@ -88,9 +89,7 @@ def open_website(message):
 	bot.send_photo(message.chat.id, img)
 	markup = types.InlineKeyboardMarkup()
 	markup.add(types.InlineKeyboardButton("Начать знакомство", url="https://leylarelax.com"))
-	bot.send_message(message.chat.id,
-			"Сотни девушек ждут вашего сообщения, напишите им!",
-			parse_mode='html', reply_markup=markup)
+	bot.send_message(message.chat.id, "Сотни девушек ждут вашего сообщения, напишите им!", parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(commands=['insta'])
 def instagram(message):
@@ -118,9 +117,9 @@ def start(message):
 	btn3 = types.KeyboardButton('Одесса')
 	btn4 = types.KeyboardButton('Харьков')
 	markup.add(btn1, btn2, btn3, btn4)
-	send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}</b>!\nСотни девушек ждут тебя, выбери свой город?"
-	img = open("main.jpg", 'rb')
-	bot.send_photo(message.chat.id, img)
+	send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}</b>!\nСотни девушек ждут тебя, выбери свой город" + url
+	#img = open("main.jpg", 'rb')
+	#bot.send_photo(message.chat.id, img)
 	bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
